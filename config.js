6b35589d4e05ceb60b7bf1eb3b21cad1f1656561
AxesArifadhy <121377989+AxesArifadhy@users.noreply.github.com>
2023-03-30 08:56:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,11 +16,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['62895604670507']
-global.ownernomer = "62895604670507"
-global.premium = ['62895604670507','6281328144300']
-global.packname = 'Zenss'
-global.author = 'Cuyy'
+global.owner = ['6285728528013']
+global.ownernomer = "6285728528013"
+global.premium = ['6285728528013','62881038387955']
+global.packname = 'AlpineBot'
+global.author = 'Axes'
 global.sessionName = 'zenss'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
